083dc837f6bad3e8c247c3d21b3f673ebf002e6f
Stephen Croll <stephen.d.croll@gmail.com>
2021-10-04 16:23:22 -0600

    Add initial benchmarks, update tests
    Changes:
    - Add initial benchmarks.
    
    - Add new README.md file with basic instructions for running tests and
      benchmarks.
    
    - Add hit/miss/eviction stats to cache.
    
    - Use testify for tests.
    
    - Update tests to not use the same values as keys.
    
    - Add test with wikipedia values.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Basic LRU Cache
+
+To run the tests, do:
+
+    make test
+
+To run the benchmarks, do:
+
+    make benchmark
--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -5,6 +5,12 @@ import (
 	"math"
 )
 
+type Stats struct {
+	Hits      int
+	Misses    int
+	Evictions int
+}
+
 type Entry struct {
 	value int
 	seq   int
@@ -14,6 +20,7 @@ type Lru struct {
 	m       map[int]*Entry
 	maxSize int
 	nextSeq int
+	stats   Stats
 }
 
 func NewLru(maxSize int) *Lru {
@@ -32,11 +39,15 @@ func (l *Lru) Put(key, value int) {
 
 	entry, ok := l.m[key]
 
+	// if key exists, update LRU sequence number
+
 	if ok {
 		entry.seq = l.nextSeq
 		l.nextSeq += 1
 		return
 	}
+
+	// key not found, add new entry
 
 	l.m[key] = &Entry{
 		value: value,
@@ -44,9 +55,13 @@ func (l *Lru) Put(key, value int) {
 	}
 	l.nextSeq += 1
 
+	// return if space available
+
 	if len(l.m) <= l.maxSize {
 		return
 	}
+
+	// no space available, find and evict least recently used
 
 	var lowKey int
 	lowSeq := math.MaxInt
@@ -58,18 +73,29 @@ func (l *Lru) Put(key, value int) {
 	}
 
 	delete(l.m, lowKey)
+	l.stats.Evictions += 1
 }
 
 func (l *Lru) Get(key int) int {
 
 	entry, ok := l.m[key]
 
+	// return failure if not in cache
+
 	if !ok {
+		l.stats.Misses += 1
 		return -1
 	}
 
+	// return cached value
+
 	entry.seq = l.nextSeq
 	l.nextSeq += 1
+	l.stats.Hits += 1
 
 	return entry.value
+}
+
+func (l *Lru) GetStats() Stats {
+	return l.stats
 }
